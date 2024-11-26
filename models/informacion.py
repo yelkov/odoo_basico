c37a5ff6049e37ed4c83b8f8216d09f7513b9e26
a23yelkovq <yelkovq@gmail.com>
2024-11-26 11:30:52 +0100

    decoradores

--- models/informacion.py
+++ models/informacion.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class odoo_basico(models.Model):
@@ -13,8 +14,42 @@ class odoo_basico(models.Model):
     ancho_en_cms = fields.Integer(string="Ancho en cm:")
     longo_en_cms = fields.Integer(string="Largo en cm:")
     peso = fields.Float(digits=(6,2),default=2.7,string="Peso en Kg:")
+    volume = fields.Float(digits=(6,7),compute="_volume", store=True, string="Volume m3")
+    densidad = fields.Float(digits=(6,2),compute="_densidad",store=True,string="Densidad Kg/m3")
     autorizado = fields.Boolean(string="¿Autorizado?",default=True)
     sexo_traducido = fields.Selection([('Hombre','Home'),('Mujer','Muller'),('Otros','Outros')],string="Sexo:")
+    literal = fields.Char(store=False)
+
+    _sql_constraints = [('nomeUnico', 'unique(name)', 'Non se pode repetir o nome')]
+    _order = "descripcion desc"
+
+    @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
+    def _volume(self):
+        for rexistro in self:
+            rexistro.volume = float(rexistro.alto_en_cms) * float(rexistro.longo_en_cms) * float(rexistro.ancho_en_cms) / 1000000
+
+    @api.depends('volume', 'peso')
+    def _densidad(self):
+        for rexistro in self:
+            if rexistro.volume != 0:
+                rexistro.densidad = float(rexistro.peso) / float(rexistro.volume)
+            else:
+                rexistro.densidad = 0.00
+
+    @api.onchange('alto_en_cms')
+    def _avisoAlto(self):
+        for rexistro in self:
+            if rexistro.alto_en_cms > 7:
+                rexistro.literal = 'O alto ten un valor posiblemente excesivo %s é maior que 7' % rexistro.alto_en_cms
+            else:
+                rexistro.literal = ""
+
+    @api.constrains('peso')  # Ao usar ValidationError temos que importar a libreria ValidationError
+    def _constrain_peso(self):   # from odoo.exceptions import ValidationError
+        for rexistro in self:
+            if rexistro.peso < 1 or rexistro.peso > 4:
+                raise ValidationError('Os peso de %s ten que ser entre 1 e 4 ' % rexistro.name)
+
 #     value2 = fields.Float(compute="_value_pc", store=True)
 #     description = fields.Text()
 #
